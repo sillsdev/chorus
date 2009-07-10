bda9cde1c4840c721da51e22615e36aab19abd64
John Hatton <hattonjohn@gmail.com>
2009-07-10 14:20:05 +1000

    Renamed projects; the old "chorus" is now "libchorus".  Baton is now "chorus"

--- src/Baton/Properties/AssemblyInfo.cs
+++ src/Baton/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Baton")]
+[assembly: AssemblyTitle("Chorus")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Baton")]
+[assembly: AssemblyCompany("SIL International")]
+[assembly: AssemblyProduct("Chorus")]
 [assembly: AssemblyCopyright("Copyright ©  2009")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
