2e3169db8df3e780f76fb850e53a698e5d0c5224
John Hatton <hattonjohn@gmail.com>
2009-09-10 16:35:50 +1000

    Add some error handling to try to figure out why Chorus getFromUSb is crashing on linux.

--- src/LibChorus/clone/CloneFromUsb.cs
+++ src/LibChorus/clone/CloneFromUsb.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
+using System.Windows.Forms;
 using Chorus.Utilities;
 using Chorus.Utilities.UsbDrive;
 using Chorus.VcsDrivers.Mercurial;
@@ -40,21 +41,47 @@ namespace Chorus.clone
 		{
 			foreach (var drive in DriveInfoRetriever.GetDrives())
 			{
-				foreach (var dir in Directory.GetDirectories(drive.RootDirectory.FullName))
+				string[] directories = new string[0];
+				try
+				{ // this is all complicated because the yield can't be inside the try/catch
+					directories = Directory.GetDirectories(drive.RootDirectory.FullName);
+				}
+				catch (Exception error)
+				{
+					MessageBox.Show(
+						string.Format("Error while looking at usb drive.  The drive root was {0}. The error was: {1}",
+									  drive.RootDirectory.FullName, error.Message), "Error", MessageBoxButtons.OK,
+						MessageBoxIcon.Error);
+				}
+				foreach (var dir in directories)
 				{
 					if (Directory.Exists(Path.Combine(dir, ".hg")) && ProjectFilter(dir))
 					{
-							yield return dir;
+						yield return dir;
 					}
 					else //we'll look just at the next level down
 					{
-						foreach (var subdir in Directory.GetDirectories(dir))
+						string[] subdirs = new string[0];
+						try
+						{    // this is all complicated because the yield can't be inside the try/catch
+							subdirs = Directory.GetDirectories(dir);
+						}
+						catch (Exception error)
+						{
+							MessageBox.Show(
+								string.Format(
+									"Error while looking at usb drive.  The drive root was {0}, the directory was {1}. The error was: {2}",
+									drive.RootDirectory.FullName, dir, error.Message), "Error", MessageBoxButtons.OK,
+								MessageBoxIcon.Error);
+						}
+						foreach (var subdir in subdirs)
 						{
 							if (Directory.Exists(Path.Combine(subdir, ".hg")) && ProjectFilter(subdir))
 							{
 								yield return subdir;
 							}
 						}
+
 					}
 				}
 			}
