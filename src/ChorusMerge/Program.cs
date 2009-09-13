cf92f838714f4c06999d74c93aa30938c9f1b9b1
John Hatton <hattonjohn@gmail.com>
2009-09-14 08:53:33 +1000

    Add message from chorusmerge that should be seen in the log from chorus (testing flow of console output on linux side)

--- src/ChorusMerge/Program.cs
+++ src/ChorusMerge/Program.cs
@@ -21,7 +21,10 @@ namespace ChorusMerge
 		{
 			try
 			{
-			  // Debug.Fail("hello");
+				//this was originally put here to test if console writes were making it out to the linux log or not
+				Console.WriteLine("ChorusMerge({0}, {1}, {2}", args[0], args[1], args[2]);
+
+				// Debug.Fail("hello");
 				MergeOrder order = MergeOrder.CreateUsingEnvironmentVariables(args[0], args[1], args[2]);
 				var handlers = ChorusFileTypeHandlerCollection.CreateWithInstalledHandlers();
 				var handler = handlers.GetHandlerForMerging(order.pathToOurs);
