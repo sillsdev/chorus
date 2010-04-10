7427182ac4b2154fae0fd316e5fcf1ac4fc61f09
Randy Regnier <rbregnier@gmail.com>
2010-04-10 15:01:19 -0500

    Initial checkin for FieldWorks 7.0 xml file handler.

---
+++ src/LibChorus/FileTypeHanders/FieldWorks/FieldWorksFileHandler.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using Chorus.merge;
+using Chorus.Utilities;
+using Chorus.VcsDrivers.Mercurial;
+
+namespace Chorus.FileTypeHanders.FieldWorks
+{
+	/// <summary>
+	/// File handler for a FieldWorks 7.0+ xml file.
+	/// </summary>
+	public class FieldWorksFileHandler : IChorusFileTypeHandler
+	{
+		#region Implementation of IChorusFileTypeHandler
+
+		public bool CanDiffFile(string pathToFile)
+		{
+			throw new NotImplementedException();
+		}
+
+		public bool CanMergeFile(string pathToFile)
+		{
+			throw new NotImplementedException();
+		}
+
+		public bool CanPresentFile(string pathToFile)
+		{
+			throw new NotImplementedException();
+		}
+
+		public bool CanValidateFile(string pathToFile)
+		{
+			throw new NotImplementedException();
+		}
+
+		/// <summary>
+		/// Do a 3-file merge, placing the result over the "ours" file and returning an error status
+		/// </summary>
+		/// <remarks>Implementations can exit with an exception, which the caller will catch and deal with.
+		/// The must not have any UI, no interaction with the user.</remarks>
+		public void Do3WayMerge(MergeOrder mergeOrder)
+		{
+			throw new NotImplementedException();
+		}
+
+		public IEnumerable<IChangeReport> Find2WayDifferences(FileInRevision parent, FileInRevision child, HgRepository repository)
+		{
+			throw new NotImplementedException();
+		}
+
+		public IChangePresenter GetChangePresenter(IChangeReport report, HgRepository repository)
+		{
+			throw new NotImplementedException();
+		}
+
+		/// <summary>
+		/// return null if valid, otherwise nice verbose description of what went wrong
+		/// </summary>
+		public string ValidateFile(string pathToFile, IProgress progress)
+		{
+			throw new NotImplementedException();
+		}
+
+		/// <summary>
+		/// This is like a diff, but for when the file is first checked in.  So, for example, a dictionary
+		/// handler might list any the words that were already in the dictionary when it was first checked in.
+		/// </summary>
+		public IEnumerable<IChangeReport> DescribeInitialContents(FileInRevision fileInRevision, TempFile file)
+		{
+			throw new NotImplementedException();
+		}
+
+		public IEnumerable<string> GetExtensionsOfKnownTextFileTypes()
+		{
+			throw new NotImplementedException();
+		}
+
+		#endregion
+	}
+}
---
+++ src/LibChorusTests/FileHandlers/FieldWorks/FieldWorksFileHandlerTests.cs
@@ -0,0 +1,110 @@
+﻿using Chorus.FileTypeHanders;
+using Chorus.FileTypeHanders.FieldWorks;
+using NUnit.Framework;
+
+namespace LibChorus.Tests.FileHandlers.FieldWorks
+{
+	/// <summary>
+	/// Test the FieldWorksFileHandler implementation of the IChorusFileTypeHandler interface.
+	/// </summary>
+	[TestFixture]
+	public class FieldWorksFileHandlerTests
+	{
+		private readonly IChorusFileTypeHandler m_handler;
+
+		public FieldWorksFileHandlerTests()
+		{
+			m_handler = new FieldWorksFileHandler();
+		}
+
+		/// <summary>
+		/// Make sure the CanDiffFile method is not implemented.
+		/// </summary>
+		[Test]
+		[ExpectedException("NotImplementedException")]
+		public void CanDiffFile_NotImplemented()
+		{
+			m_handler.CanDiffFile("bogusPathname");
+		}
+
+		/// <summary>
+		/// Make sure the CanMergeFile method is not implemented.
+		/// </summary>
+		[Test]
+		[ExpectedException("NotImplementedException")]
+		public void CanMergeFile_NotImplemented()
+		{
+			m_handler.CanMergeFile("bogusPathname");
+		}
+
+		/// <summary>
+		/// Make sure the CanPresentFile method is not implemented.
+		/// </summary>
+		[Test]
+		[ExpectedException("NotImplementedException")]
+		public void CanPresentFile_NotImplemented()
+		{
+			m_handler.CanPresentFile("bogusPathname");
+		}
+
+		/// <summary>
+		/// Make sure the CanValidateFile method is not implemented.
+		/// </summary>
+		[Test]
+		[ExpectedException("NotImplementedException")]
+		public void CanValidateFile_NotImplemented()
+		{
+			m_handler.CanValidateFile("bogusPathname");
+		}
+
+		/// <summary>
+		/// Make sure the Do3WayMerge method is not implemented.
+		/// </summary>
+		[Test]
+		[ExpectedException("NotImplementedException")]
+		public void Do3WayMerge_NotImplemented()
+		{
+			m_handler.Do3WayMerge(null);
+		}
+
+		/// <summary>
+		/// Make sure the GetChangePresenter method is not implemented.
+		/// </summary>
+		[Test]
+		[ExpectedException("NotImplementedException")]
+		public void GetChangePresenter_NotImplemented()
+		{
+			m_handler.GetChangePresenter(null, null);
+		}
+
+		/// <summary>
+		/// Make sure the ValidateFile method is not implemented.
+		/// </summary>
+		[Test]
+		[ExpectedException("NotImplementedException")]
+		public void ValidateFile_NotImplemented()
+		{
+			m_handler.ValidateFile("bogusPathname", null);
+		}
+
+		/// <summary>
+		/// Make sure the DescribeInitialContents method is not implemented.
+		/// </summary>
+		[Test]
+		[ExpectedException("NotImplementedException")]
+		public void DescribeInitialContents_NotImplemented()
+		{
+			m_handler.DescribeInitialContents(null, null);
+		}
+
+		/// <summary>
+		/// Make sure the GetExtensionsOfKnownTextFileTypes method is not implemented.
+		/// </summary>
+		[Test]
+		[ExpectedException("NotImplementedException")]
+		public void GetExtensionsOfKnownTextFileTypes_NotImplemented()
+		{
+			m_handler.GetExtensionsOfKnownTextFileTypes();
+		}
+	}
+}
