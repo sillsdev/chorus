09cb4396b914a668deb19e4e8a4acce592d0f792
Randy Regnier <rbregnier@gmail.com>
2010-04-27 11:16:40 -0500

    Refactor ElementReader to get rid of some duplicate code. Comment out some now unused methods in LiftUtils.

--- src/LibChorus/Utilities/ElementReader.cs
+++ src/LibChorus/Utilities/ElementReader.cs
@@ -11,35 +11,6 @@ namespace Chorus.Utilities
 	/// </summary>
 	public class ElementReader : IDisposable
 	{
-		/* Sample handler/delegate Action:
-		private static readonly byte[] s_guid = Encoding.UTF8.GetBytes("guid=\"");
-		private static readonly byte[] s_class = Encoding.UTF8.GetBytes("class=\"");
-		private static readonly byte s_close = Encoding.UTF8.GetBytes("\"")[0];
-
-		void MakeSurrogate(byte[] xmlBytes)
-		{
-			var surrogate = m_surrogateFactory.Create(
-				CmObjectId.Create(new Guid(GetAttribute(s_guid, xmlBytes))),
-				GetAttribute(s_class, xmlBytes),
-				xmlBytes);
-			if (m_needConversion)
-				RegisterSurrogateForConversion(surrogate);
-			else
-				RegisterInactiveSurrogate(surrogate);
-		}
-
-		string GetAttribute(byte[] name, byte[] input)
-		{
-			int start = input.IndexOfSubArray(name);
-			if (start == -1)
-				return null;
-			start += name.Length;
-			int end = Array.IndexOf(input, s_close, start);
-			if (end == -1)
-				return null; // error
-			return Encoding.UTF8.GetString(input.SubArray(start, end - start));
-		}
-		*/
 		private byte[] m_inputData;
 		private readonly byte[] m_openingMarker;
 		private readonly byte[] m_finalClosingTag;
@@ -65,6 +36,9 @@ namespace Chorus.Utilities
 		{
 			TrimInput();
 
+			if (m_startOfRecordsOffset == m_endOfRecordsOffset)
+				return; // Nothing to do.
+
 			var openingAngleBracket = m_openingMarker[0];
 			for (var i = m_startOfRecordsOffset; i < m_endOfRecordsOffset; ++i)
 			{
@@ -82,35 +56,7 @@ namespace Chorus.Utilities
 		private void TrimInput()
 		{
 			// Trim off junk at the start.
-			var openingAngleBracket = m_openingMarker[0];
-			var canStop = false;
-			for (var i = 0; i < m_endOfRecordsOffset; ++i)
-			{
-				var currentByte = m_inputData[i];
-				// Need to get the next starting marker, or the main closing tag
-				// When the end point is found, call m_outputHandler with the current array
-				// from 'offset' to 'i' (more or less).
-				// Skip quickly over anything that doesn't match even one character.
-				if (currentByte != openingAngleBracket)
-					continue; // Useless check for any xml input that starts with the normal <?xml version='1.0' encoding='utf-8'?>
-
-				// Try to match the rest of the marker.
-				for (var j = 1; ; j++)
-				{
-					if (m_openingMarker[j] != m_inputData[i + j])
-						break; // no match, resume searching for opening character.
-					if (j != m_openingMarker.Length - 1)
-						continue;
-
-					// Got it!
-					m_startOfRecordsOffset = i;
-					canStop = true;
-					break;
-				}
-				if (canStop)
-					break;
-			}
-
+			m_startOfRecordsOffset = FindStartOfElement(0, m_openingMarker[0]);
 			// Trim off end tag. It really better be the last bunch of bytes!
 			m_endOfRecordsOffset = m_inputData.Length - m_finalClosingTag.Length;
 		}
