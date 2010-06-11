a57337e09a346552dd9f4516ee204419fd392843
Randy Regnier <rbregnier@gmail.com>
2010-06-10 23:36:29 -0500

    Refactor FieldWorksMerger class.

--- src/LibChorus/FileTypeHanders/FieldWorks/FieldWorksMerger.cs
+++ src/LibChorus/FileTypeHanders/FieldWorks/FieldWorksMerger.cs
@@ -11,7 +11,7 @@ namespace Chorus.FileTypeHanders.FieldWorks
 	/// <summary>
 	/// Class that does the bulk of the 3-way merging for FieldWorks 7.0 data.
 	/// </summary>
-	public class FieldWorksMerger
+	internal class FieldWorksMerger
 	{
 		private readonly MergeOrder _mergeOrder;
 		private readonly IMergeStrategy _mergeStategy;
@@ -20,7 +20,7 @@ namespace Chorus.FileTypeHanders.FieldWorks
 		private string _loserXml;
 		private string _commonAncestorXml;
 
-		public FieldWorksMerger(MergeOrder mergeOrder, IMergeStrategy mergeStategy, string pathToWinner, string pathToLoser, string pathToCommonAncestor, string winnerId)
+		internal FieldWorksMerger(MergeOrder mergeOrder, IMergeStrategy mergeStategy, string pathToWinner, string pathToLoser, string pathToCommonAncestor, string winnerId)
 		{
 			_mergeOrder = mergeOrder;
 			_mergeStategy = mergeStategy;
@@ -32,62 +32,14 @@ namespace Chorus.FileTypeHanders.FieldWorks
 		}
 
 		/// <summary>Used by tests, which prefer to give us raw contents rather than paths</summary>
-		public FieldWorksMerger(string winnerXml, string loserXml, string commonAncestorXml)
+		internal FieldWorksMerger(string winnerXml, string loserXml, string commonAncestorXml)
 		{
 			_winnerXml = winnerXml;
 			_loserXml = loserXml;
 			_commonAncestorXml = commonAncestorXml;
 		}
 
-		private void Do2WayDiff(string parentXml, ref string childXml,
-			IDictionary<string, XmlNode> goners, IDictionary<string, ChangedElement> dirtballs, IDictionary<string, XmlNode> newbies)
-		{
-			var winnerCommonListener = new ChangeAndConflictAccumulator();
-			var winnerDiffer = Xml2WayDiffer.CreateFromStrings(
-				parentXml,
-				childXml,
-				winnerCommonListener,
-				"rt",
-				"languageproject",
-				"guid");
-			childXml = null;
-			try
-			{
-				winnerDiffer.ReportDifferencesToListener();
-				foreach (var winnerDif in winnerCommonListener.Changes)
-				{
-					if (!(winnerDif is IXmlChangeReport))
-						continue; // It could be ErrorDeterminingChangeReport, so what to do with it?
-
-					var asXmlReport = (IXmlChangeReport)winnerDif;
-					switch (winnerDif.GetType().Name)
-					{
-						case "XmlDeletionChangeReport":
-							var gonerNode = asXmlReport.ParentNode;
-							goners.Add(gonerNode.Attributes["guid"].Value, gonerNode);
-							break;
-						case "XmlChangedRecordReport":
-							var originalNode = asXmlReport.ParentNode;
-							var updatedNode = asXmlReport.ChildNode;
-							dirtballs.Add(originalNode.Attributes["guid"].Value, new ChangedElement
-							{
-								m_parentNode = originalNode,
-								m_childNode = updatedNode
-							});
-							break;
-						case "XmlAdditionChangeReport":
-							var newbieNode = asXmlReport.ChildNode;
-							newbies.Add(newbieNode.Attributes["guid"].Value, newbieNode);
-							break;
-					}
-				}
-			}
-			// ReSharper disable EmptyGeneralCatchClause
-			catch { }
-			// ReSharper restore EmptyGeneralCatchClause
-		}
-
-		public void DoMerge(string outputPathname)
+		internal void DoMerge(string outputPathname)
 		{
 			var newbies = new Dictionary<string, XmlNode>();
 			// Do diff between winner and common
@@ -136,9 +88,59 @@ namespace Chorus.FileTypeHanders.FieldWorks
 
 					WriteOutNewObjects(newbies, enc, writer);
 
+// ReSharper disable PossibleNullReferenceException
 					writer.WriteEndElement();
+// ReSharper restore PossibleNullReferenceException
 				}
 			}
+		}
+
+		private static void Do2WayDiff(string parentXml, ref string childXml,
+			IDictionary<string, XmlNode> goners, IDictionary<string, ChangedElement> dirtballs, IDictionary<string, XmlNode> newbies)
+		{
+			var winnerCommonListener = new ChangeAndConflictAccumulator();
+			var winnerDiffer = Xml2WayDiffer.CreateFromStrings(
+				parentXml,
+				childXml,
+				winnerCommonListener,
+				"rt",
+				"languageproject",
+				"guid");
+			childXml = null;
+			try
+			{
+				winnerDiffer.ReportDifferencesToListener();
+				foreach (var winnerDif in winnerCommonListener.Changes)
+				{
+					if (!(winnerDif is IXmlChangeReport))
+						continue; // It could be ErrorDeterminingChangeReport, so what to do with it?
+
+					var asXmlReport = (IXmlChangeReport)winnerDif;
+					switch (winnerDif.GetType().Name)
+					{
+						case "XmlDeletionChangeReport":
+							var gonerNode = asXmlReport.ParentNode;
+							goners.Add(gonerNode.Attributes["guid"].Value, gonerNode);
+							break;
+						case "XmlChangedRecordReport":
+							var originalNode = asXmlReport.ParentNode;
+							var updatedNode = asXmlReport.ChildNode;
+							dirtballs.Add(originalNode.Attributes["guid"].Value, new ChangedElement
+							{
+								m_parentNode = originalNode,
+								m_childNode = updatedNode
+							});
+							break;
+						case "XmlAdditionChangeReport":
+							var newbieNode = asXmlReport.ChildNode;
+							newbies.Add(newbieNode.Attributes["guid"].Value, newbieNode);
+							break;
+					}
+				}
+			}
+			// ReSharper disable EmptyGeneralCatchClause
+			catch { }
+			// ReSharper restore EmptyGeneralCatchClause
 		}
 
 		private void ProcessMainRecordElements(IDictionary<string, XmlNode> winnerGoners,
@@ -168,46 +170,7 @@ namespace Chorus.FileTypeHanders.FieldWorks
 				if (winnerDirtballs.ContainsKey(currentGuid))
 				{
 					transferUntouched = false;
-					if (loserGoners.ContainsKey(currentGuid))
-					{
-						// Winner edited it, but loser deleted it.
-						// Make a conflict report.
-						var dirtballElement = winnerDirtballs[currentGuid];
-						EventListener.ConflictOccurred(new EditedVsRemovedElementConflict(
-														"rt",
-														dirtballElement.m_childNode,
-														loserGoners[currentGuid],
-														dirtballElement.m_parentNode,
-														_mergeOrder.MergeSituation,
-														new ElementStrategy(false),
-														_winnerId));
-
-						ReplaceCurrentNode(writer, dirtballElement.m_childNode);
-						winnerDirtballs.Remove(currentGuid);
-						loserGoners.Remove(currentGuid);
-					}
-					else
-					{
-						if (loserDirtballs.ContainsKey(currentGuid))
-						{
-							// Both edited it. Check it out.
-							var mergedResult = winnerDirtballs[currentGuid].m_childNode.OuterXml;
-							if (!XmlUtilities.AreXmlElementsEqual(winnerDirtballs[currentGuid].m_childNode, loserDirtballs[currentGuid].m_childNode))
-							{
-								var dirtballElement = winnerDirtballs[currentGuid];
-								mergedResult = _mergeStategy.MakeMergedEntry(EventListener, dirtballElement.m_childNode,
-																			 loserDirtballs[currentGuid].m_childNode, dirtballElement.m_parentNode);
-							}
-							ReplaceCurrentNode(writer, mergedResult);
-							loserDirtballs.Remove(currentGuid);
-						}
-						else
-						{
-							// Winner edited it. Loser did nothing with it.
-							ReplaceCurrentNode(writer, winnerDirtballs[currentGuid].m_childNode);
-							winnerDirtballs.Remove(currentGuid);
-						}
-					}
+					ProcessWinnerEditedRecord(currentGuid, loserGoners, loserDirtballs, winnerDirtballs, writer);
 				}
 
 				if (loserGoners.ContainsKey(currentGuid))
@@ -216,6 +179,7 @@ namespace Chorus.FileTypeHanders.FieldWorks
 					// If winner had either deleted or edited it,
 					// then the code above here would have been involved,
 					// and currentGuid would have been removed from loserGoners.
+					// The net effect is that it will be removed.
 					transferUntouched = false;
 					loserGoners.Remove(currentGuid);
 				}
@@ -223,13 +187,13 @@ namespace Chorus.FileTypeHanders.FieldWorks
 				{
 					// Loser changed it, but winner did nothing to it.
 					transferUntouched = false;
-					ReplaceCurrentNode(writer, loserDirtballs[currentGuid].m_childNode);
-					loserDirtballs.Remove(currentGuid);
+					ReplaceCurrentNode(writer, loserDirtballs, currentGuid);
 				}
 
 				if (!transferUntouched)
 				{
-					// Read to next <rt> element
+					// Read to next <rt> element,
+					// Which skips writinng our the current element.
 					reader.ReadOuterXml();
 					keepReading = reader.IsStartElement();
 					continue;
@@ -238,6 +202,56 @@ namespace Chorus.FileTypeHanders.FieldWorks
 				// Nobody did anything with the current source node, so just copy it to output.
 				writer.WriteNode(reader, false);
 				keepReading = reader.IsStartElement();
+			}
+		}
+
+		private static void ReplaceCurrentNode(XmlWriter writer, IDictionary<string, ChangedElement> loserDirtballs, string currentGuid)
+		{
+			ReplaceCurrentNode(writer, loserDirtballs[currentGuid].m_childNode);
+			loserDirtballs.Remove(currentGuid);
+		}
+
+		private void ProcessWinnerEditedRecord(string currentGuid, IDictionary<string, XmlNode> loserGoners, IDictionary<string, ChangedElement> loserDirtballs, IDictionary<string, ChangedElement> winnerDirtballs, XmlWriter writer)
+		{
+			if (loserGoners.ContainsKey(currentGuid))
+			{
+				// Winner edited it, but loser deleted it.
+				// Make a conflict report.
+				var dirtballElement = winnerDirtballs[currentGuid];
+				EventListener.ConflictOccurred(new EditedVsRemovedElementConflict(
+												"rt",
+												dirtballElement.m_childNode,
+												loserGoners[currentGuid],
+												dirtballElement.m_parentNode,
+												_mergeOrder.MergeSituation,
+												new ElementStrategy(false),
+												_winnerId));
+
+				ReplaceCurrentNode(writer, dirtballElement.m_childNode);
+				winnerDirtballs.Remove(currentGuid);
+				loserGoners.Remove(currentGuid);
+			}
+			else
+			{
+				if (loserDirtballs.ContainsKey(currentGuid))
+				{
+					// Both edited it. Check it out.
+					var mergedResult = winnerDirtballs[currentGuid].m_childNode.OuterXml;
+					if (!XmlUtilities.AreXmlElementsEqual(winnerDirtballs[currentGuid].m_childNode, loserDirtballs[currentGuid].m_childNode))
+					{
+						var dirtballElement = winnerDirtballs[currentGuid];
+						mergedResult = _mergeStategy.MakeMergedEntry(EventListener, dirtballElement.m_childNode,
+																	 loserDirtballs[currentGuid].m_childNode, dirtballElement.m_parentNode);
+					}
+					ReplaceCurrentNode(writer, mergedResult);
+					loserDirtballs.Remove(currentGuid);
+				}
+				else
+				{
+					// Winner edited it. Loser did nothing with it.
+					ReplaceCurrentNode(writer, winnerDirtballs[currentGuid].m_childNode);
+					winnerDirtballs.Remove(currentGuid);
+				}
 			}
 		}
 
@@ -330,7 +344,7 @@ namespace Chorus.FileTypeHanders.FieldWorks
 			}
 		}
 
-		public IMergeEventListener EventListener
+		internal IMergeEventListener EventListener
 		{ get; set; }
 
 		private class ChangedElement
