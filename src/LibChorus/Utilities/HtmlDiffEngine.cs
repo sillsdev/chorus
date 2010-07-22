62da029ef5d5eb89cad5ac7a46813ee86c1c2f27
Bob Eaton <bob_barb_eaton@yahoo.com>
2010-07-21 20:50:00 -0500

    made the Begin/EndTag's public so I can use them in some code outside of the class

--- src/LibChorus/Utilities/HtmlDiffEngine.cs
+++ src/LibChorus/Utilities/HtmlDiffEngine.cs
@@ -115,7 +115,7 @@ namespace Rainbow.HtmlDiffEngine
 	/// for marking up the deleted words in the merged
 	/// file.
 	/// </summary>
-	class CommentOff
+	public class CommentOff
 	{
 		static public string BeginTag = "<span style=\"text-decoration: line-through; color: red\">";
 		static public string EndTag = "</span>";
@@ -126,7 +126,7 @@ namespace Rainbow.HtmlDiffEngine
 	/// for marking up the added words in the merged
 	/// file.
 	/// </summary>
-	class Added
+	public class Added
 	{
 		static public string BeginTag = "<span style=\"background: Yellow\">";
 		static public string EndTag = "</span>";
