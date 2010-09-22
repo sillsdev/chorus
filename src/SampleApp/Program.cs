2a80cb3a0da41f1f1987f5263c53aab31cdbf03b
John Hatton <hattonjohn@gmail.com>
2010-09-22 11:39:51 +1000

    Start of a sample app.

---
+++ src/SampleApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using Chorus.sync;
+using Chorus.Utilities;
+using Palaso.IO;
+
+namespace SampleApp
+{
+	static class Program
+	{
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+
+			var path = @"c:\dev\temp\testChorusData";
+			ProjectFolderConfiguration project = new ProjectFolderConfiguration(path);
+			var synchronizer = new Chorus.sync.Synchronizer(path, project, new NullProgress());
+			synchronizer.SyncNow(new SyncOptions());
+			//Application.Run(new Form1());
+		}
+	}
+}
