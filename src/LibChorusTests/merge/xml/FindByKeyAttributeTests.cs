be9c2584dd55b6bbad79d20699166cbd3e197364
Cambell Prince <cambell_prince@sil.org>
2011-01-19 10:51:19 +0700

    Fix WS-33895 Exception thrown in xpath FindByKeyAttribute

---
+++ src/LibChorusTests/merge/xml/FindByKeyAttributeTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using Chorus.merge.xml.generic;
+using NUnit.Framework;
+
+namespace LibChorus.Tests.merge.xml
+{
+	[TestFixture]
+	public sealed class FindByKeyAttributeTests
+	{
+		// A regression test: http://jira.palaso.org/issues/browse/WS-33895
+		// Some lift ids have single quotes in them, so the xpath needs to be escaped correctly.
+		[Test]
+		public void GetNodeToMerge_WithSingleQuoteInAttribute_NoThrow()
+		{
+			string xml =
+				@"<lift>
+					<entry id=""te'st"" />
+				</lift>";
+
+			var doc1 = new XmlDocument();
+			doc1.LoadXml(xml);
+
+			var finder = new FindByKeyAttribute("id");
+			var node = doc1.SelectSingleNode("//entry");
+			var result = finder.GetNodeToMerge(node, doc1.DocumentElement);
+			Assert.NotNull(result);
+			XmlTestHelper.AssertXPathMatchesExactlyOne(result, "entry[@id=\"te'st\"]");
+		}
+
+		[Test]
+		public void GetNodeToMerge_ReturnsNode()
+		{
+			string xml =
+				@"<lift>
+					<entry id='test' />
+				</lift>";
+
+			var doc1 = new XmlDocument();
+			doc1.LoadXml(xml);
+
+			var finder = new FindByKeyAttribute("id");
+			var node = doc1.SelectSingleNode("//entry");
+			var result = finder.GetNodeToMerge(node, doc1.DocumentElement);
+			Assert.NotNull(result);
+			XmlTestHelper.AssertXPathMatchesExactlyOne(result, "entry[@id=\"test\"]");
+		}
+	}
+
+}
