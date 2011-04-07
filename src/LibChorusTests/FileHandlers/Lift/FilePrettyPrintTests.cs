b30cb6ece6570421d6c79aad66a3d5f51de9bb55
Randy Regnier <rbregnier@gmail.com>
2011-04-07 10:14:06 -0500

    Refactor the Lift file 'pretty print' code into Chorus, so it can be shared by the two Lift Bridges.

---
+++ src/LibChorusTests/FileHandlers/Lift/FilePrettyPrintTests.cs
@@ -0,0 +1,256 @@
+﻿using System.IO;
+using Chorus.FileTypeHanders.lift;
+using NUnit.Framework;
+using Palaso.IO;
+
+namespace LibChorus.Tests.FileHandlers.Lift
+{
+	[TestFixture]
+	public class FilePrettyPrintTests
+	{
+		[Test]
+		public void ShouldMaintainCorrectOrderOfEntries()
+		{
+			const string parent =
+@"<?xml version='1.0' encoding='utf-8'?>
+<lift
+	version='0.13'
+	producer='SIL.FLEx 7.0.0.40590'>
+	<entry
+		id='pos1' />
+	<entry
+		id='pos2' />
+	<entry
+		id='pos3' />
+</lift>";
+			const string child =
+@"<?xml version='1.0' encoding='utf-8'?><lift version='0.13' producer='SIL.FLEx 7.0.0.40590'><entry id='pos3'/><entry id='pos1'/><entry id='pos2'/></lift>";
+
+			using (var tempParent = new TempFile(parent))
+			using (var tempChild = new TempFile(child))
+			{
+				LiftFileServices.PrettyPrintFile(tempParent.Path, tempChild.Path);
+				var newOutputContents = File.ReadAllText(tempParent.Path);
+				Assert.AreEqual(parent.Replace("'", "\""), newOutputContents); // Correct order and format.
+			}
+		}
+
+		[Test]
+		public void NewHeaderShouldBeAdded()
+		{
+			const string parent =
+@"<?xml version='1.0' encoding='utf-8'?>
+<lift
+	version='0.13'
+	producer='SIL.FLEx 7.0.0.40590'>
+	<entry
+		id='pos1' />
+	<entry
+		id='pos2' />
+	<entry
+		id='pos3' />
+</lift>";
+			const string child =
+@"<?xml version='1.0' encoding='utf-8'?>
+<lift
+	version='0.13'
+	producer='SIL.FLEx 7.0.0.40590'><header><stuff></stuff></header>
+	<entry
+		id='pos1' />
+	<entry
+		id='pos2' />
+	<entry
+		id='pos3' />
+</lift>";
+
+			const string wellformedHeader =
+@"	<header>
+		<stuff></stuff>
+	</header>";
+
+			using (var tempParent = new TempFile(parent))
+			using (var tempChild = new TempFile(child))
+			{
+				LiftFileServices.PrettyPrintFile(tempParent.Path, tempChild.Path);
+				var newOutputContents = File.ReadAllText(tempParent.Path);
+				Assert.IsTrue(newOutputContents.Contains(wellformedHeader)); // Header added with good format.
+			}
+		}
+
+		[Test]
+		public void ChangedHeaderShouldBeAdded()
+		{
+			const string parent =
+@"<?xml version='1.0' encoding='utf-8'?>
+<lift
+	version='0.13'
+	producer='SIL.FLEx 7.0.0.40590'>
+	<header>
+		<stuff></stuff>
+	</header>
+	<entry
+		id='pos1' />
+	<entry
+		id='pos2' />
+	<entry
+		id='pos3' />
+</lift>";
+			const string child =
+@"<?xml version='1.0' encoding='utf-8'?>
+<lift
+	version='0.13'
+	producer='SIL.FLEx 7.0.0.40590'><header><newstuff></newstuff></header>
+	<entry
+		id='pos1' />
+	<entry
+		id='pos2' />
+	<entry
+		id='pos3' />
+</lift>";
+
+			const string replacedHeader =
+@"	<header>
+		<newstuff></newstuff>
+	</header>";
+
+			using (var tempParent = new TempFile(parent))
+			using (var tempChild = new TempFile(child))
+			{
+				LiftFileServices.PrettyPrintFile(tempParent.Path, tempChild.Path);
+				var newOutputContents = File.ReadAllText(tempParent.Path);
+				Assert.IsTrue(newOutputContents.Contains(replacedHeader)); // Header changed with good format.
+			}
+		}
+
+		[Test]
+		public void CaseShouldNotMatterInChildIds()
+		{
+			const string parent =
+@"<?xml version='1.0' encoding='utf-8'?>
+<lift
+	version='0.13'
+	producer='SIL.FLEx 7.0.0.40590'>
+	<header>
+		<stuff></stuff>
+	</header>
+	<entry
+		id='pos1' />
+	<entry
+		id='pos2' />
+	<entry
+		id='pos3' />
+</lift>";
+			const string child =
+@"<?xml version='1.0' encoding='utf-8'?>
+<lift
+	version='0.13'
+	producer='SIL.FLEx 7.0.0.40590'>
+	<header>
+		<stuff></stuff>
+	</header>
+	<entry
+		id='pos1' />
+	<entry
+		id='POS2' />
+	<entry
+		id='pos3' />
+</lift>";
+
+			using (var tempParent = new TempFile(parent))
+			using (var tempChild = new TempFile(child))
+			{
+				LiftFileServices.PrettyPrintFile(tempParent.Path, tempChild.Path);
+				var newOutputContents = File.ReadAllText(tempParent.Path);
+				Assert.IsTrue(newOutputContents.Contains("POS2"));
+			}
+		}
+
+		[Test]
+		public void CaseShouldNotMatterInParentIds()
+		{
+			const string parent =
+@"<?xml version='1.0' encoding='utf-8'?>
+<lift
+	version='0.13'
+	producer='SIL.FLEx 7.0.0.40590'>
+	<header>
+		<stuff></stuff>
+	</header>
+	<entry
+		id='pos1' />
+	<entry
+		id='POS2' />
+	<entry
+		id='pos3' />
+</lift>";
+			const string child =
+@"<?xml version='1.0' encoding='utf-8'?>
+<lift
+	version='0.13'
+	producer='SIL.FLEx 7.0.0.40590'>
+	<header>
+		<stuff></stuff>
+	</header>
+	<entry
+		id='pos1' />
+	<entry
+		id='pos2' />
+	<entry
+		id='pos3' />
+</lift>";
+
+			using (var tempParent = new TempFile(parent))
+			using (var tempChild = new TempFile(child))
+			{
+				LiftFileServices.PrettyPrintFile(tempParent.Path, tempChild.Path);
+				var newOutputContents = File.ReadAllText(tempParent.Path);
+				Assert.IsTrue(newOutputContents.Contains("pos2"));
+			}
+		}
+
+		[Test]
+		public void RemovedHeaderShouldNotBePresent()
+		{
+			const string parent =
+@"<?xml version='1.0' encoding='utf-8'?>
+<lift
+	version='0.13'
+	producer='SIL.FLEx 7.0.0.40590'>
+	<header>
+		<stuff></stuff>
+	</header>
+	<entry
+		id='pos1' />
+	<entry
+		id='pos2' />
+	<entry
+		id='pos3' />
+</lift>";
+			const string child =
+@"<?xml version='1.0' encoding='utf-8'?>
+<lift
+	version='0.13'
+	producer='SIL.FLEx 7.0.0.40590'>
+	<entry
+		id='pos1' />
+	<entry
+		id='pos2' />
+	<entry
+		id='pos3' />
+</lift>";
+
+			const string removedHeader =
+@"	<header>
+		<stuff></stuff>
+	</header>";
+
+			using (var tempParent = new TempFile(parent))
+			using (var tempChild = new TempFile(child))
+			{
+				LiftFileServices.PrettyPrintFile(tempParent.Path, tempChild.Path);
+				var newOutputContents = File.ReadAllText(tempParent.Path);
+				Assert.IsFalse(newOutputContents.Contains(removedHeader)); // Header removed.
+			}
+		}
+	}
+}
