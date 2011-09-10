2b5c11acb91ea7f5bb9dce19cb2c1274881105fd
Cambell Prince <cambell_prince@sil.org>
2011-09-10 16:33:26 +0700

    Add build/build.win.proj

--- build/TestInstallerBuild.bat
+++ build/TestInstallerBuild.bat
@@ -1,7 +1,7 @@
-call "c:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\vcvarsall.bat"
+call "c:\Program Files (x86)\Microsoft Visual Studio 8.0\VC\vcvarsall.bat"
 
-pushd c:\src\sil\solid\build
-MSbuild /target:installer /property:teamcity_build_checkoutDir=c:\src\sil\solid  /property:teamcity_dotnet_nunitlauncher_msbuild_task="notthere" /property:BUILD_NUMBER="0.1.345.abcd" /property:Minor="1"
+pushd c:\src\sil\chorus\build
+MSbuild /target:UnzipMercurial /property:teamcity_build_checkoutDir=c:\src\sil\chorus /property:teamcity_dotnet_nunitlauncher_msbuild_task="notthere" /property:BUILD_NUMBER="0.1.345.abcd" /property:Minor="1" build.win.proj
 popd
 PAUSE
 
