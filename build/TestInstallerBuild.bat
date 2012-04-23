ac302752978ade54b122b5dd6d2cd27767a8c092
Cambell Prince <cambell_prince@sil.org>
2012-04-23 14:04:41 +0700

    Update build for installer, exclude mercurial.ini from generation.

--- build/TestInstallerBuild.bat
+++ build/TestInstallerBuild.bat
@@ -1,7 +1,7 @@
 call "c:\Program Files (x86)\Microsoft Visual Studio 8.0\VC\vcvarsall.bat"
 
 pushd c:\src\sil\chorus-installer\build
-MSbuild /target:MakeWixForDistFiles /property:teamcity_build_checkoutDir=c:\src\sil\chorus-installer /property:teamcity_dotnet_nunitlauncher_msbuild_task="notthere" /property:BUILD_NUMBER="0.1.345.abcd" /property:Minor="1" build.win.proj
+MSbuild /target:%1 /property:teamcity_build_checkoutDir=c:\src\sil\chorus-installer /property:teamcity_dotnet_nunitlauncher_msbuild_task="notthere" /property:BUILD_NUMBER="0.1.345.abcd" /property:Minor="1" build.win.proj
 popd
 PAUSE
 
