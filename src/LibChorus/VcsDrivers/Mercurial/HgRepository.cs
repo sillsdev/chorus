5ab1c393520c41f72f12b558da67977a31e6b9ed
Daniel Glassey <daniel_glassey@sil.org>
2012-04-25 14:22:48 +0700

    don't enable win32text extension for user on linux

--- src/LibChorus/VcsDrivers/Mercurial/HgRepository.cs
+++ src/LibChorus/VcsDrivers/Mercurial/HgRepository.cs
@@ -209,7 +209,9 @@ namespace Chorus.VcsDrivers.Mercurial
 			try
 			{
 				var extensions = new Dictionary<string, string>();
+#if !MONO
 				extensions.Add("hgext.win32text", ""); //for converting line endings on windows machines
+#endif
 				extensions.Add("hgext.graphlog", ""); //for more easily readable diagnostic logs
 				extensions.Add("convert", ""); //for catastrophic repair in case of repo corruption
 #if !MONO
