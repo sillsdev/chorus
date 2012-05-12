110e98219f77841de33279a0d5e5bfa5b057ff73
Randy Regnier <rbregnier@gmail.com>
2012-05-12 14:43:31 -0500

    Have chorus notes file use canonical writer settings after merge and delete empty notes files, if nothing added.

--- src/LibChorusTests/merge/XmlLogMergeEventListenerTests.cs
+++ src/LibChorusTests/merge/XmlLogMergeEventListenerTests.cs
@@ -71,9 +71,9 @@ namespace LibChorus.Tests.merge
 		[Test]
 		public void FileDidNotExist_CreatesCorrectFile()
 		{
-			using (TempFile logFile =  TempFile.CreateAndGetPathButDontMakeTheFile())
+			using (TempFile logFile = TempFile.CreateAndGetPathButDontMakeTheFile())
 			{
-				using(ChorusNotesMergeEventListener log = new ChorusNotesMergeEventListener(logFile.Path))
+				using (ChorusNotesMergeEventListener log = new ChorusNotesMergeEventListener(logFile.Path))
 				{
 					log.ConflictOccurred(new DummyConflict());
 					log.ConflictOccurred(new DummyConflict());
@@ -85,17 +85,14 @@ namespace LibChorus.Tests.merge
 		}
 
 		[Test]
-		public void FileDidNotExist_NoConflicts_CreatesCorrectFile()
+		public void FileDidNotExist_NoConflicts_DoesNotCreatesEmptyNotesFile()
 		{
-			using (TempFile logFile = TempFile.CreateAndGetPathButDontMakeTheFile())
+			var tempFile = Path.GetTempFileName();
+			File.Delete(tempFile);
+			using (ChorusNotesMergeEventListener log = new ChorusNotesMergeEventListener(tempFile))
 			{
-				using (ChorusNotesMergeEventListener log = new ChorusNotesMergeEventListener(logFile.Path))
-				{
-				 }
-				XmlDocument doc = new XmlDocument();
-				doc.Load(logFile.Path);
-				Assert.AreEqual(1, doc.SelectNodes("notes").Count);
 			}
+			Assert.IsFalse(File.Exists(tempFile));
 		}
 
 	}
