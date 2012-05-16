fdaa080a7361a7e96165235280a920afd800e068
Alistair Imrie <alistair_underscore_imrie_at@sil_dot.org>
2012-05-16 14:23:33 -0500

    Nearly working with BackgroundWorker object.

--- src/Chorus/UI/Clone/GetCloneFromNetworkFolderDlg.cs
+++ src/Chorus/UI/Clone/GetCloneFromNetworkFolderDlg.cs
@@ -1,24 +1,89 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
+using System.IO;
 using System.Windows.Forms;
 
 namespace Chorus.UI.Clone
 {
 	public partial class GetCloneFromNetworkFolderDlg : Form
 	{
+		private GetCloneFromNetworkFolderModel _model;
+		private BackgroundWorker _backgroundWorker;
+
 		public GetCloneFromNetworkFolderDlg()
 		{
 			InitializeComponent();
+
+			InitializeBackgroundWorker();
+		}
+
+		private void InitializeBackgroundWorker()
+		{
+			_backgroundWorker = new BackgroundWorker();
+			_backgroundWorker.DoWork += _backgroundWorker_DoWork;
+			// _backgroundWorker.RunWorkerCompleted
+			_backgroundWorker.ProgressChanged += _backgroundWorker_ProgressChanged;
+
+		}
+
+		private void _backgroundWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+		{
+			throw new NotImplementedException();
+		}
+
+		private void _backgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+		{
+			// Initiate recursive searching for Hg repositories:
+			FillRepositoryList(_model.FolderPath, _backgroundWorker);
+		}
+
+		public void LoadFromModel(GetCloneFromNetworkFolderModel model)
+		{
+			_model = model;
+			folderBrowserControl.SelectedPath = _model.FolderPath;
 		}
 
 		private void folderBrowserControl_PathChanged(object sender, EventArgs e)
 		{
-			statusLabel.Text = folderBrowserControl.SelectedPath;
+			if (_model == null)
+				throw new InvalidDataException(@"_model not initialized. Call LoadFromModel() in GetCloneFromNetworkFolderDlg object before displaying dialog.");
+
+			_model.FolderPath = folderBrowserControl.SelectedPath;
+
+			_backgroundWorker.RunWorkerAsync();
+		}
+
+		private void FillRepositoryList(string folderPath, BackgroundWorker worker)
+		{
+			statusLabel.Text = folderPath;//silly!
+
+			if (worker.CancellationPending)
+			{
+				// Bail-out base case:
+				return;
+			}
+
+			if (_model.IsValidRepository(folderPath))
+			{
+				// Add folderPath details to projectRepositoryListView:
+				var folderItem = new ListViewItem();
+				folderItem.SubItems.Add(Path.GetFileName(folderPath));
+				folderItem.SubItems.Add(File.GetLastWriteTime(folderPath).ToString());
+				projectRepositoryListView.Items.Add(folderItem);
+				// Main base case:
+				return;
+			}
+
+			if (!_model.IsFolderWorthSearching(folderPath))
+			{
+				// Bail-out base case:
+				return;
+			}
+
+			var subFolders = Directory.GetDirectories(folderPath);
+			foreach (var subFolder in subFolders)
+				FillRepositoryList(subFolder, worker);
 		}
 
 		private void panel_Resize(object sender, EventArgs e)
---
+++ src/Chorus/UI/Clone/GetCloneFromNetworkFolderModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Chorus.VcsDrivers.Mercurial;
+using Palaso.Progress.LogBox;
+
+namespace Chorus.UI.Clone
+{
+	public class GetCloneFromNetworkFolderModel
+	{
+		///<summary>
+		/// This serves as both the initial folder to present/search, and also the selected repository path.
+		///</summary>
+		public string FolderPath { get; set; }
+
+		public IEnumerable<string> GetDirectoriesWithMecurialRepos()
+		{
+			throw new NotImplementedException();
+		}
+
+		///<summary>
+		/// Makes a Mercurial clone of a repository from sourcePath to parentDirectoryToPutCloneIn
+		///</summary>
+		///<param name="sourcePath">Existing Hg repo</param>
+		///<param name="parentDirectoryToPutCloneIn">Target folder for new clone</param>
+		///<param name="progress">Progress indicator object</param>
+		///<returns>Directory that clone was actually placed in (allows for renaming to avoid duplicates)</returns>
+		public string MakeClone(string sourcePath, string parentDirectoryToPutCloneIn, IProgress progress)
+		{
+			return HgHighLevel.MakeCloneFromLocalToLocal(sourcePath,
+														 Path.Combine(parentDirectoryToPutCloneIn, Path.GetFileName(sourcePath)),
+														 true,
+														 progress);
+		}
+
+		/// <summary>
+		/// Decides if the given folder path is worth the hassle of examining for Hg repositories.
+		/// </summary>
+		/// <param name="path"></param>
+		/// <returns>true if a search is a good idea</returns>
+		public bool IsFolderWorthSearching(string path)
+		{
+			return true;
+		}
+
+		public bool IsValidRepository(string folderPath)
+		{
+			return Directory.Exists(folderPath + Path.DirectorySeparatorChar + @".hg");
+		}
+	}
+}
