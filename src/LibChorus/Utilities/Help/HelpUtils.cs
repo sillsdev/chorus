4e23e607b88f4538720337caa2d04a5d04be0af4
JFMcCoy <devnull@localhost>
2012-07-11 15:46:07 -0500

    Added a preliminary help utility to Chorus (and unit test).

---
+++ src/LibChorus/Utilities/Help/HelpUtils.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Chorus.Utilities.Help
+{
+	///<summary>
+	/// Utilities for storing the embedded Help File on disk and retrieving it.
+	///</summary>
+	public class HelpUtils
+	{
+		/// <summary>
+		/// Extracts the embedded help file and returns a filepath to it.
+		/// </summary>
+		/// <returns>The filepath to the help file.</returns>
+		public static string GetHelpFile()
+		{
+			var helpFile = Application.StartupPath + @"\Send_Receive_Help.chm";
+
+			// if (!File.Exists(helpFile))
+			ExtractHelpFile();
+
+			return helpFile;
+		}
+
+		/// <summary>
+		/// Extracts the embedded help file.
+		/// </summary>
+		public static void ExtractHelpFile()
+		{
+			var helpFile = Application.StartupPath + @"\Send_Receive_Help.chm";
+
+			var assembly = Assembly.GetExecutingAssembly();
+			var stream = assembly.GetManifestResourceStream("Chorus.Send_Receive_Help.chm");
+			if (stream == null)
+			{
+				throw new NullReferenceException(); // ?
+			}
+			var binReader = new BinaryReader(stream);
+			var binWriter = new BinaryWriter(File.Open(helpFile, FileMode.Create));
+
+			var i = 0;
+			while (i < stream.Length)
+			{
+				binWriter.Write(binReader.ReadByte());
+				i++;
+			}
+			binReader.Close();
+			binWriter.Close();
+
+			if (!File.Exists(helpFile))
+			{
+				throw new FileNotFoundException("The help file could not be retrieved");
+			}
+		}
+	}
+}
---
+++ src/LibChorusTests/utilities/Help/HelpUtilitiesTests.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using Chorus.Utilities.Help;
+using NUnit.Framework;
+
+namespace LibChorus.Tests.utilities.Help
+{
+	[TestFixture]
+	public class HelpUtilitiesTests
+	{
+		[Test]
+		public void Stream_to_Helpfile_Exists()
+		{
+			Assert.IsNotNull(Assembly.GetAssembly(typeof (HelpUtils)).GetManifestResourceStream("Chorus.Send_Receive_Help.chm"));
+		}
+	}
+}
