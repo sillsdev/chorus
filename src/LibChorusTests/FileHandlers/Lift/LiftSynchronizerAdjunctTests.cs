e2016f5914a3501422aa9635a7ccd4ba44d47c70
Gordon Martin <gordon-soft_martin@sil.org>
2012-08-23 15:45:19 -0700

    One last clean-up.

--- src/LibChorusTests/FileHandlers/Lift/LiftSynchronizerAdjunctTests.cs
+++ src/LibChorusTests/FileHandlers/Lift/LiftSynchronizerAdjunctTests.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Chorus.FileTypeHanders.lift;
+﻿using Chorus.FileTypeHanders.lift;
 using NUnit.Framework;
 using Palaso.IO;
 
