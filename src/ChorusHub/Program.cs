4b8ac284cbe60b10f948fff2bf12f6be4556a87f
John Hatton <hattonjohn@gmail.com>
2012-10-01 17:51:36 +1000

    Fix palaso reference of ChorusHub to Release folder

--- src/ChorusHub/Program.cs
+++ src/ChorusHub/Program.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Runtime.InteropServices;
-using System.ServiceProcess;
-using System.Text;
 using System.Threading;
 
 namespace ChorusHub
