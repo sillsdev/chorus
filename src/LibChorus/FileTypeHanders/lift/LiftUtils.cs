08f12b558a818dd71274373994290f1f1aee48f0
Randy Regnier <rbregnier@gmail.com>
2013-02-12 08:37:15 -0600

    Tweak code a bit more and add comments that explain issues related to the &label business.

--- src/LibChorus/FileTypeHanders/lift/LiftUtils.cs
+++ src/LibChorus/FileTypeHanders/lift/LiftUtils.cs
@@ -32,8 +32,8 @@ namespace Chorus.FileTypeHanders.lift
 
 		public static string GetUrl(XmlNode entryNode, string fileNameUnescaped)
 		{
-			fileNameUnescaped = fileNameUnescaped==null?"unknown": Uri.EscapeDataString(fileNameUnescaped);
-			string url = string.Format("lift://{0}?type=entry&", fileNameUnescaped);
+			var fileNameEscaped = (fileNameUnescaped == null) ? "unknown" : Uri.EscapeDataString(fileNameUnescaped);
+			string url = string.Format("lift://{0}?type=entry&", fileNameEscaped);
 
 			var guid = GetGuid(entryNode);
 			if (!string.IsNullOrEmpty(guid))
@@ -49,10 +49,10 @@ namespace Chorus.FileTypeHanders.lift
 				}
 			}
 
-
 			var form = GetFormForEntry(entryNode);
 			if (!string.IsNullOrEmpty(form))
 			{
+				// Q: Should this be "$form=", since the caller wants to add its own "&label=" part of the query?
 				url += "label=" + Uri.EscapeDataString(form) + "&";
 			}
 			url = url.Trim('&');
@@ -61,12 +61,16 @@ namespace Chorus.FileTypeHanders.lift
 
 		public static string GetUrl(XmlNode child, string unescaped, string label)
 		{
-			var x = GetUrl(child, unescaped);
+			var url = GetUrl(child, unescaped);
 			if(string.IsNullOrEmpty(label))
-				return x;
-			if (!x.Contains("&label="))
-				x = x + "&label=" + Uri.EscapeDataString(label);
-			return x;
+				return url;
+
+			// The call, above, to GetUrl, adds "&label=", and the provided "label" returns the same 'form' of the entry in its xpath,
+			// as is used in the above call to GetUrl.
+			// Do URLs support two duplicate parts of the query? Even if the contents of "&label=" are not the same, is that supported?
+			if (!url.Contains("&label="))
+				url = url + "&label=" + Uri.EscapeDataString(label);
+			return url;
 		}
 	}
 }
