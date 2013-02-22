7c686edd183d22c5aab8043d811480f147fb6e2e
John Thomson <john_thomson@sil.org>
2013-02-22 14:03:57 -0600

    Obtain from internet/chorushub now deletes repo if it has same hash as one we already have. A new dialog reports this. The logic for initializing the lists of repos we already have has moved down into Chorus. The client can specify an extended message in this dialog explaining how to Send/Receive the existing project in case that is what the user intended.
    There is an API change to GetSharedProjectModel.GetSharedProjectUsing().
    
    One more fix is needed to pass the right help identifier once it is created
    in Chorus_Help.chm

---
+++ src/Chorus/UI/Clone/DuplicateProjectWarningDialog.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Chorus.Utilities.Help;
+
+namespace Chorus.UI.Clone
+{
+	public partial class DuplicateProjectWarningDialog : Form
+	{
+		public DuplicateProjectWarningDialog()
+		{
+			InitializeComponent();
+		}
+
+		public void Run(string projectWithExistingRepo, string howToSendReceiveMessageText)
+		{
+			// Review JohnH: this is probably not generic enough to use for WeSay, Bloom, and other clients.
+			// If we need to fix it
+			_mainLabel.Text = string.Format(
+				"The project \"{0}\" on this computer is already using this repository. {1}",
+				projectWithExistingRepo, howToSendReceiveMessageText);
+			ShowDialog();
+		}
+
+
+		private void buttonHelp_Click(object sender, EventArgs e)
+		{
+			Help.ShowHelp(this, HelpUtils.GetHelpFile(),
+					"Tasks/Internet_tab.htm"); // Todo: replace with real address when topic created.
+		}
+	}
+}
