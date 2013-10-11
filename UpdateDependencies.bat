89b44f1293a2ce3d914b6dbf497549a720e36447
Randy Regnier <rbregnier@gmail.com>
2013-10-11 11:36:04 -0700

    Update GetAndBuildThis and UpdateDependencies batch files

--- UpdateDependencies.bat
+++ UpdateDependencies.bat
@@ -1,33 +1,30 @@
 REM Since Palaso libraries change frequently, you will likely need to
 REM get that project and be able to build it, then run this script.
 REM This script assumes that the libraries project are on the same level as this project.
-REM It copies the needed libraries both into the lib folder and the debug folder.
+REM It copies the needed libraries both into the lib folder and the debug/release folder.
+
+IF "%1"=="" (
+	set BUILD_CONFIG="Debug"
+) ELSE (
+	set BUILD_CONFIG=%1
+)
 
 pushd .
-
 cd ..\palaso
-call GetAndBuildThis.bat
+call GetAndBuildThis.bat %BUILD_CONFIG% %2
 popd
 
-copy /Y ..\palaso\output\debug\palaso.dll lib\debug\
-copy /Y ..\palaso\output\debug\palaso.xml lib\debug\
-copy /Y ..\palaso\output\debug\palaso.pdb lib\debug\
+copy /Y ..\palaso\output\%BUILD_CONFIG%\palaso.dll lib\%BUILD_CONFIG%
+copy /Y ..\palaso\output\%BUILD_CONFIG%\palaso.pdb lib\%BUILD_CONFIG%
 
-copy /Y ..\palaso\output\debug\Palaso.Lift.dll lib\debug\
-copy /Y ..\palaso\output\debug\Palaso.Lift.xml lib\debug\
-copy /Y ..\palaso\output\debug\Palaso.Lift.pdb lib\debug\
+copy /Y ..\palaso\output\%BUILD_CONFIG%\Palaso.Lift.dll lib\%BUILD_CONFIG%
+copy /Y ..\palaso\output\%BUILD_CONFIG%\Palaso.Lift.pdb lib\%BUILD_CONFIG%
 
-copy /Y ..\palaso\output\debug\palasouiwindowsforms.dll  lib\debug
-copy /Y ..\palaso\output\debug\palasouiwindowsforms.xml  lib\debug
-copy /Y ..\palaso\output\debug\palasouiwindowsforms.pdb  lib\debug
+copy /Y ..\palaso\output\%BUILD_CONFIG%\palasouiwindowsforms.dll  lib\%BUILD_CONFIG%
+copy /Y ..\palaso\output\%BUILD_CONFIG%\palasouiwindowsforms.pdb  lib\%BUILD_CONFIG%
 
 
-copy /Y ..\palaso\output\debug\palaso.testutilities.dll lib\debug
-copy /Y ..\palaso\output\debug\palaso.testutilities.xml lib\debug
-copy /Y ..\palaso\output\debug\palaso.testutilities.pdb debug
-
-copy /Y ..\palaso\output\debug\palaso.*  output\debug
-copy /Y ..\palaso\output\debug\palaso.testutilities.*  output\debug
-copy /Y ..\palaso\output\debug\palasouiwindowsforms.*  output\debug
+copy /Y ..\palaso\output\%BUILD_CONFIG%\palaso.testutilities.dll lib\%BUILD_CONFIG%
+copy /Y ..\palaso\output\%BUILD_CONFIG%\palaso.testutilities.pdb lib\%BUILD_CONFIG%
 
 pause
