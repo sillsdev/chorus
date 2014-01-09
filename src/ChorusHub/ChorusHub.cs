5387b0a3f4fb53f7ef95706d41b00ff395a83ae2
Randy Regnier <rbregnier@gmail.com>
2014-01-09 13:26:45 -0700

    Add do-nothing Windows Service project

---
+++ src/ChorusHub/ChorusHub.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.ServiceProcess;
+using System.Text;
+
+namespace ChorusHub
+{
+	public partial class ChorusHub : ServiceBase
+	{
+		public ChorusHub()
+		{
+			InitializeComponent();
+
+			CanPauseAndContinue = false;
+		}
+
+		protected override void OnStart(string[] args)
+		{
+			EventLog.WriteEntry("In OnStart");
+		}
+
+		protected override void OnStop()
+		{
+			EventLog.WriteEntry("In OnStop");
+		}
+	}
+}
---
+++ src/ChorusHub/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ServiceProcess;
+using System.Text;
+
+namespace ChorusHub
+{
+	static class Program
+	{
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		static void Main()
+		{
+			var servicesToRun = new ServiceBase[] 
+			{ 
+				new ChorusHub() 
+			};
+			ServiceBase.Run(servicesToRun);
+		}
+	}
+}
