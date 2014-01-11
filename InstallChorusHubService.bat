d2d4b95acecde819fec46451d34d842fab710e73
Randy Regnier <rbregnier@gmail.com>
2014-01-11 12:27:31 -0700

    Add batch files to add/remove CH service on local machine

---
+++ InstallChorusHubService.bat
@@ -0,0 +1,14 @@
+setlocal
+
+IF "%1"=="" (
+	set BUILD_CONFIG="Debug"
+) ELSE (
+	set BUILD_CONFIG=%1
+)
+
+IF "%2"=="" (
+	call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"
+	SET HaveCalledvcvarsall=True
+)
+
+installutil.exe .\output\%BUILD_CONFIG%\ChorusHub.exe
