cfb82084ca264ac3184499102bf57e86638158b6
Chris Hubbard <chris_hubbard@sil.org>
2014-02-12 10:24:20 -0500

    Make parameter to TestBuild.sh same as chorus
    * use Debug or Release (instead of DebugMono ReleaseMono)

--- build/TestBuild.sh
+++ build/TestBuild.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
+cd "$(dirname "$0")"
 
-if [ "$1"=="" ]
-then
-		BUILD_CONFIG=Debug
-else
-		BUILD_CONFIG=$1
-fi
-
-
-xbuild "/target:Clean;Compile" /property:Configuration=${BUILD_CONFIG}Mono /property:RootDir=.. /property:BUILD_NUMBER="1.5.1.abcd" build.mono.proj
+xbuild "/target:Clean;Compile" /property:Configuration="${1:-Debug}Mono" /property:RootDir=.. /property:BUILD_NUMBER="1.5.1.abcd" build.mono.proj
