fcb00c83bd51bbe6feaf9f9c851dab01413ae39e
Tim Steenwyk <tim_steenwyk@sil.org>
2014-11-04 14:36:45 -0500

    (TimS, MikeL): Chorus Hub: Fixed getting revision of file when it was not changed in the tip revision. Chorus Hub: Optimized getting repository information when IDs are not needed (1000x speed increase).

--- src/LibChorus/ChorusHub/IChorusHubService.cs
+++ src/LibChorus/ChorusHub/IChorusHubService.cs
@@ -22,12 +22,30 @@ namespace Chorus.ChorusHub
 		/// </summary>
 		/// <example>searchUrl: "scheme://path?filePattern=*.lift|*.CustomProperties"</example>
 		/// <example>returned repo info string: {"name": "someProject", "id": "123abc"}</example>
-		/// <param name="searchUrl"></param>
-		/// <returns></returns>
 		[OperationContract]
 		string GetRepositoryInformation(string searchUrl);
 
-		/// <summary>
+        /// <summary>
+        /// Returns information about the Hg repositories that the ChorusHub knows about.
+        ///
+        /// The search can be trimmed by use of the 'searchUrl' parameter.
+        /// Everything about the searchUrl except the query string is fabricated
+        /// by the ChorusHubClient. The query string is fed in by the application.
+        ///
+        /// The possible search values are:
+        /// filePattern -- This key can have multiple values separated by the '|' character
+        ///
+        /// Each repository generates a JSON string consisting of two name/value pairs.
+        /// The two names are "name" and "id". The JSON strings are concatenated, separated by /.
+        /// (An earlier version returned an enumeration of json strings. But Mono could not
+        /// marshal this.)
+        /// </summary>
+        /// <example>searchUrl: "scheme://path?filePattern=*.lift|*.CustomProperties"</example>
+        /// <example>returned repo info string: {"name": "someProject", "id": "123abc"}</example>
+        [OperationContract]
+        string GetRepositoryInformationWithoutIds(string searchUrl);
+        
+        /// <summary>
 		/// Prepares to receive a repository by checking that it exists on the server, and if not,
 		/// creating a new directory with that name.
 		/// </summary>
