67c3b867827dc8662e328817012ba6683958eca8
Eberhard Beilharz <eb1@sil.org>
2015-02-18 17:48:32 +0100

    Make use of new Chorus.proj file

--- build/TestBuild.sh
+++ build/TestBuild.sh
@@ -2,4 +2,4 @@
 cd "$(dirname "$0")/.."
 root=$PWD
 cd build
-xbuild "/target:${2:-Clean;Compile}" /property:Configuration="${1:-Debug}Mono" /property:RootDir=$root /property:BUILD_NUMBER="1.5.1.abcd" build.mono.proj
+xbuild "/target:${2:-Clean;Compile}" /property:Configuration="${1:-Debug}Mono" /property:RootDir=$root /property:BUILD_NUMBER="1.5.1.abcd" Chorus.proj
