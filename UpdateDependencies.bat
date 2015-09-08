44b9e786feb1fbd1f2f340ef690fdb4fb2a497e5
Hasso Pape <hasso_pape@sil.org>
2015-09-08 13:19:54 -0500

    Update Locally-Built Dependencies Only If They Exist

--- UpdateDependencies.bat
+++ UpdateDependencies.bat
@@ -3,14 +3,17 @@ REM get that project and be able to build it, then run this script.
 REM This script assumes that the libraries project are on the same level as this project.
 REM It copies the needed libraries both into the lib folder and the debug/release folder.
 
+set PALASO_DIR="..\libpalaso"
+
+IF NOT EXIST %PALASO_DIR% GOTO :EOF
+
 IF "%1"=="" (
 	set BUILD_CONFIG="Debug"
 ) ELSE (
 	set BUILD_CONFIG=%1
 )
 
-pushd .
-cd ..\libpalaso
+pushd %PALASO_DIR%
 REM Presence of a second argument indicates that the caller has already run vsvars32.bat
 call GetAndBuildThis.bat %BUILD_CONFIG% %2
 popd
@@ -22,10 +25,10 @@ REM copy /Y ..\l10nsharp\output\%BUILD_CONFIG%\L10NSharp.dll lib\common\
 REM copy /Y ..\l10nsharp\output\%BUILD_CONFIG%\L10NSharp.* lib\%BUILD_CONFIG%\
 REM copy /Y ..\l10nsharp\output\%BUILD_CONFIG%\L10NSharp.* output\%BUILD_CONFIG%\
 
-copy /Y ..\libpalaso\output\%BUILD_CONFIG%\Palaso*.dll lib\%BUILD_CONFIG%\
-copy /Y ..\libpalaso\output\%BUILD_CONFIG%\Palaso*.dll output\%BUILD_CONFIG%\
-copy /Y ..\libpalaso\output\%BUILD_CONFIG%\Palaso*.pdb output\%BUILD_CONFIG%\
+copy /Y %PALASO_DIR%\output\%BUILD_CONFIG%\Palaso*.dll lib\%BUILD_CONFIG%\
+copy /Y %PALASO_DIR%\output\%BUILD_CONFIG%\Palaso*.dll output\%BUILD_CONFIG%\
+copy /Y %PALASO_DIR%\output\%BUILD_CONFIG%\Palaso*.pdb output\%BUILD_CONFIG%\
 
-copy /Y ..\libpalaso\output\%BUILD_CONFIG%\SIL.*.dll lib\%BUILD_CONFIG%\
-copy /Y ..\libpalaso\output\%BUILD_CONFIG%\SIL.*.dll output\%BUILD_CONFIG%\
-copy /Y ..\libpalaso\output\%BUILD_CONFIG%\SIL.*.pdb output\%BUILD_CONFIG%\
+copy /Y %PALASO_DIR%\output\%BUILD_CONFIG%\SIL.*.dll lib\%BUILD_CONFIG%\
+copy /Y %PALASO_DIR%\output\%BUILD_CONFIG%\SIL.*.dll output\%BUILD_CONFIG%\
+copy /Y %PALASO_DIR%\output\%BUILD_CONFIG%\SIL.*.pdb output\%BUILD_CONFIG%\
