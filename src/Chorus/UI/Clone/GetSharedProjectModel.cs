71455755719a16d4edb204fae7fb6d44698f3899
Eberhard Beilharz <eb1@sil.org>
2015-11-13 16:04:51 +0100

    Move CloneResult to LibChorus
    (cherry picked from commit 91a0e23f4b8a0bb6104c0cad0a8ef630b846dc5c)

--- src/Chorus/UI/Clone/GetSharedProjectModel.cs
+++ src/Chorus/UI/Clone/GetSharedProjectModel.cs
@@ -237,24 +237,6 @@ namespace Chorus.UI.Clone
 	}
 
 	/// <summary>
-	/// The results of a clone attempt.
-	/// </summary>
-	public class CloneResult
-	{
-		/// <summary>Constructor</summary>
-		public CloneResult(string actualLocation, CloneStatus cloneStatus)
-		{
-			ActualLocation = actualLocation;
-			CloneStatus = cloneStatus;
-		}
-
-		/// <summary>Get the actual location of a clone. (May, or may not, be the same as the desired location.)</summary>
-		public string ActualLocation { get; private set; }
-		/// <summary>Get the status of the clone attempt.</summary>
-		public CloneStatus CloneStatus { get; private set; }
-	}
-
-	/// <summary>
 	/// An enumeration of the possible repository sources.
 	/// </summary>
 	public enum ExtantRepoSource
@@ -267,16 +249,4 @@ namespace Chorus.UI.Clone
 		ChorusHub
 	}
 
-	/// <summary>
-	/// An indication of the success/failure of the clone attempt.
-	/// </summary>
-	public enum CloneStatus
-	{
-		/// <summary>The clone was made</summary>
-		Created,
-		/// <summary>The clone operation was cancelled</summary>
-		Cancelled,
-		/// <summary>The clone could not be created</summary>
-		NotCreated
-	}
 }
