586f73b2efdee49e850c8eb82c1732bd0c1577be
Eberhard Beilharz <eb1@sil.org>
2018-02-09 23:14:10 +0100

    Use GuidProvider and DateTimeProvider
    This improves the testability of code that involves Chorus.

---
+++ src/LibChorus/merge/DefaultChangeReport.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) 2008-2018 SIL International
+// This software is licensed under the MIT License (http://opensource.org/licenses/MIT)
+using System;
+using Chorus.VcsDrivers.Mercurial;
+
+namespace Chorus.merge
+{
+	public class DefaultChangeReport : ChangeReport
+	{
+		private readonly string _label;
+
+		public DefaultChangeReport(FileInRevision initial, string label)
+			: base(null, initial)
+		{
+			_label = label;
+		}
+
+		public DefaultChangeReport(FileInRevision parent, FileInRevision child, string label)
+			: base(parent, child)
+		{
+			_label = label;
+		}
+
+		public override string ActionLabel
+		{
+			get { return _label; }
+		}
+	}
+}
+
