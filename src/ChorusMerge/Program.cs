99ee6158cfb82d60ef3617b93a053d154878d1ca
Eberhard Beilharz <eb1@sil.org>
2019-04-11 09:23:18 +0200

    Use sdky-style csproj files

--- src/ChorusMerge/Program.cs
+++ src/ChorusMerge/Program.cs
@@ -6,9 +6,10 @@ using System.Text;
 using Chorus.FileTypeHandlers;
 using Chorus.merge;
 using Chorus.merge.xml.generic;
+using SIL.PlatformUtilities;
 
 // Allow redirecting Console.Error for unit tests (Avoid spurious build failures)
-[assembly: InternalsVisibleTo("ChorusMerge.Tests")]
+[assembly: InternalsVisibleTo("ChorusMerge.Tests, PublicKey=00240000048000009400000006020000002400005253413100040000010001005f4452c387d979e3cba05fd73bb9aebe8f8830874663d66a7869f614a8f5e8def658d5c5920fae609d28aa005d5a9af5bd758ca8f19ad0347b7aa76e1f723f8994792136f5ceff9fb6f719d4337f65da2e1d66a85cc5e28e4656a1a30c2ff513440393177625c725d3fb156dc3c11610ea5936b9404ab9d51f7eb71ac0aa27bd")]
 
 namespace ChorusMerge
 {
