41f0e859da9f0ad1990075d60d027a7211d7bf5e
Eberhard Beilharz <eb1@sil.org>
2020-01-14 10:20:51 +0100

    Use SIL.Chorus.Mercurial nuget package

--- build/TestBuild.sh
+++ build/TestBuild.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 cd "$(dirname "$0")/.."
-build/buildupdate.mono.sh
 . environ
-root=$PWD
 cd build
-xbuild "/target:${2:-Clean;Compile}" /property:Configuration="${1:-Debug}Mono" /property:RootDir=$root /property:BUILD_NUMBER="1.5.1.abcd" Chorus.proj
+msbuild "/target:${2:-Clean;Compile}" /property:Configuration="${1:-Debug}" Chorus.proj
