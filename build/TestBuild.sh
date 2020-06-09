a78b32754a9b80804bab21a7e1ce287107592107
Eberhard Beilharz <eb1@sil.org>
2020-06-09 15:26:28 +0200

    Don't source environ when running with mono 6
    This fixes the LGTM analysis on GitHub.

--- build/TestBuild.sh
+++ build/TestBuild.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 cd "$(dirname "$0")/.."
-. environ
+if [[ $(mono --version 2>/dev/null | head -1 | cut -f 5 -d ' ') < 6 ]]; then
+	. environ
+fi
 cd build
-msbuild "/target:${2:-Clean;Compile}" /property:Configuration="${1:-Debug}" Chorus.proj
+msbuild "/target:${2:-Clean;Build}" /property:Configuration="${1:-Debug}" Chorus.proj
