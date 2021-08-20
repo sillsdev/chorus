ef08bf8d44fe29492ce0ca50e85a9140e5a70739
Robin Munn <rmunn@pobox.com>
2021-08-20 16:18:01 +0700

    Address review comments
    Make ExecuteErrorsOk an instance method since there's no reason it needs
    to be static (no code that uses the Chorus library can be calling it
    since it's private, so this won't affect any other code). This also
    allows simplifying its type signature.

--- src/LibChorus/VcsDrivers/Mercurial/HgRepository.cs
+++ src/LibChorus/VcsDrivers/Mercurial/HgRepository.cs
@@ -465,7 +465,7 @@ namespace Chorus.VcsDrivers.Mercurial
 
 		private string GetTextFromQuery(string query, int secondsBeforeTimeoutOnLocalOperation)
 		{
-			var result = ExecuteErrorsOk(query, _pathToRepository, secondsBeforeTimeoutOnLocalOperation, _progress, _hgLogCache);
+			var result = ExecuteErrorsOk(query, secondsBeforeTimeoutOnLocalOperation);
 
 			var standardOutputText = result.StandardOutput.Trim();
 			if (!string.IsNullOrEmpty(standardOutputText))
@@ -562,7 +562,7 @@ namespace Chorus.VcsDrivers.Mercurial
 			}
 			var hgCmdArgs = b.ToString();
 
-			ExecutionResult result = ExecuteErrorsOk(hgCmdArgs, _pathToRepository, secondsBeforeTimeout, _progress, _hgLogCache);
+			ExecutionResult result = ExecuteErrorsOk(hgCmdArgs, secondsBeforeTimeout);
 			if (HgProcessOutputReader.kCancelled == result.ExitCode)
 			{
 				_progress.WriteWarning("User Cancelled");
@@ -669,9 +669,9 @@ namespace Chorus.VcsDrivers.Mercurial
 		}
 
 		/// <exception cref="System.TimeoutException"/>
-		private static ExecutionResult ExecuteErrorsOk(string command, string fromDirectory, int secondsBeforeTimeout, IProgress progress, IDictionary<string, ExecutionResult> hgLogCache)
+		private ExecutionResult ExecuteErrorsOk(string command, int secondsBeforeTimeout)
 		{
-			if (progress.CancelRequested)
+			if (_progress.CancelRequested)
 			{
 				throw new UserCancelledException();
 			}
@@ -681,23 +681,23 @@ namespace Chorus.VcsDrivers.Mercurial
 #if DEBUG
 			if (GetHasLocks(fromDirectory, progress))
 			{
-				progress.WriteWarning("Found a lock before executing: {0}.", commandToLog);
+				_progress.WriteWarning("Found a lock before executing: {0}.", commandToLog);
 			}
 #endif
 
 			ExecutionResult result;
 			// The only commands safe to cache are `hg log -rREVNUM --template "{node}"`, as their output never changes for a given repo.
-			if (hgLogCache != null && command != null && command.StartsWith("log -r") && command.EndsWith("--template \"{node}\"")) {
-				if (hgLogCache.TryGetValue(command, out result)) {
-					progress.WriteVerbose("Using cached result: " + commandToLog);
+			if (_hgLogCache != null && command != null && command.StartsWith("log -r") && command.EndsWith("--template \"{node}\"")) {
+				if (_hgLogCache.TryGetValue(command, out result)) {
+					_progress.WriteVerbose("Using cached result: " + commandToLog);
 				} else {
-					progress.WriteVerbose("Executing and caching: " + commandToLog);
-					result = HgRunner.Run("hg " + commandToLog, fromDirectory, secondsBeforeTimeout, progress);
-					hgLogCache[command] = result;
+					_progress.WriteVerbose("Executing and caching: " + commandToLog);
+					result = HgRunner.Run("hg " + commandToLog, _pathToRepository, secondsBeforeTimeout, _progress);
+					_hgLogCache[command] = result;
 				}
 			} else {
-				progress.WriteVerbose("Executing: " + commandToLog);
-				result = HgRunner.Run("hg " + commandToLog, fromDirectory, secondsBeforeTimeout, progress);
+				_progress.WriteVerbose("Executing: " + commandToLog);
+				result = HgRunner.Run("hg " + commandToLog, _pathToRepository, secondsBeforeTimeout, _progress);
 			}
 			if (result.DidTimeOut)
 			{
@@ -709,18 +709,18 @@ namespace Chorus.VcsDrivers.Mercurial
 			}
 			if (!string.IsNullOrEmpty(result.StandardError))
 			{
-				progress.WriteVerbose("standerr: " + result.StandardError);//not necessarily an *error* down this deep
+				_progress.WriteVerbose("standerr: " + result.StandardError);//not necessarily an *error* down this deep
 			}
 			if (!string.IsNullOrEmpty(result.StandardOutput))
 			{
-				progress.WriteVerbose("standout: " + result.StandardOutput);//not necessarily an *error* down this deep
+				_progress.WriteVerbose("standout: " + result.StandardOutput);//not necessarily an *error* down this deep
 			}
 
 #if DEBUG
 			//nb: store/lock is so common with recover (in hg 1.3) that we don't even want to mention it
 			if (!commandToLog.Contains("recover") && GetHasLocks(fromDirectory, progress))
 			{
-				progress.WriteWarning("{0} left a lock.", commandToLog);
+				_progress.WriteWarning("{0} left a lock.", commandToLog);
 			}
 #endif
 			return result;
@@ -1224,7 +1224,7 @@ namespace Chorus.VcsDrivers.Mercurial
 			var f = TempFile.WithExtension(Path.GetExtension(relativePath));
 
 			var cmd = string.Format("cat -o \"{0}\" -r {1} \"{2}\"", f.Path, revOrHash, relativePath);
-			ExecutionResult result = ExecuteErrorsOk(cmd, _pathToRepository, SecondsBeforeTimeoutOnLocalOperation, _progress, _hgLogCache);
+			ExecutionResult result = ExecuteErrorsOk(cmd, SecondsBeforeTimeoutOnLocalOperation);
 			if (!string.IsNullOrEmpty(result.StandardError.Trim()))
 			{
 				// At least zap the temp file, since it isn't to be returned.
@@ -1618,7 +1618,7 @@ namespace Chorus.VcsDrivers.Mercurial
 		{
 
 			// No longer uses "hg incoming", since that takes just as long as a pull, according to the hg mailing list
-			//    ExecutionResult result = ExecuteErrorsOk(string.Format("incoming -l 1 {0}", SurroundWithQuotes(uri)), _pathToRepository, SecondsBeforeTimeoutOnLocalOperation, _progress, _hgLogCache);
+			//    ExecutionResult result = ExecuteErrorsOk(string.Format("incoming -l 1 {0}", SurroundWithQuotes(uri)), SecondsBeforeTimeoutOnLocalOperation);
 			//so we're going to just ping
 
 			try
@@ -2222,7 +2222,7 @@ namespace Chorus.VcsDrivers.Mercurial
 		public void FixUnicodeAudio()
 		{
 			CheckAndUpdateHgrc();
-			ExecuteErrorsOk("addremove -s 100 -I **.wav", _pathToRepository, SecondsBeforeTimeoutOnLocalOperation, _progress, _hgLogCache);
+			ExecuteErrorsOk("addremove -s 100 -I **.wav", SecondsBeforeTimeoutOnLocalOperation);
 		}
 
 		private static string GetUniqueFolderPath(IProgress progress, string proposedTargetDirectory)
