0eeaacd8dbe490d14178c25e0b953463b3668146
Robin Munn <rmunn@pobox.com>
2021-08-20 17:49:18 +0700

    Don't cache all-zero results from hg
    An all-zero result from hg is highly likely to be the repo identifier of
    an empty repository, which is going to change in the near future. So
    we'll skip caching anything that starts with twelve zeroes in a row.
    This will occasionally have a false positive, but that false positive
    won't have any negative impact (a minor slowdown).

--- src/LibChorus/VcsDrivers/Mercurial/HgRepository.cs
+++ src/LibChorus/VcsDrivers/Mercurial/HgRepository.cs
@@ -693,7 +693,11 @@ namespace Chorus.VcsDrivers.Mercurial
 				} else {
 					_progress.WriteVerbose("Executing and caching: " + commandToLog);
 					result = HgRunner.Run("hg " + commandToLog, _pathToRepository, secondsBeforeTimeout, _progress);
-					_hgLogCache[command] = result;
+					if (!string.IsNullOrEmpty(result.StandardOutput) && result.StandardOutput.StartsWith("000000000000")) {
+						_progress.WriteVerbose("Not caching an all-zero result");
+					} else {
+						_hgLogCache[command] = result;
+					}
 				}
 			} else {
 				_progress.WriteVerbose("Executing: " + commandToLog);
