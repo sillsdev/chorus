e7ab5bcca7ae571374eb9c8d742a1975aec453f3
Eberhard Beilharz <eb1@sil.org>
2022-08-26 09:53:24 +0200

    Remove `build/` subdirectory
    Removing `build/` subdirectory since we're building with GHA
    now. `TestInstallerBuild.bat` (and the targets it calls in
    `Chorus.proj`) might still be relevant, but are terribly outdated
    so that it no longer works.

--- build/TestBuild.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd "$(dirname "$0")/.."
-if [[ $(mono --version 2>/dev/null | head -1 | cut -f 5 -d ' ') < 6 ]]; then
-	. environ
-fi
-cd build
-msbuild "/target:${2:-Clean;Build}" /property:Configuration="${1:-Debug}" Chorus.proj
